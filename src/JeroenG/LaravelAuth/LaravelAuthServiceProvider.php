348d0933c76ff26fecc1bb09f2732db739efcbe8
JeroenG <jeroengjeroeng@gmail.com>
2014-03-03 18:48:52 +0100

    Updated serviceprovider with auth extension

--- src/JeroenG/LaravelAuth/LaravelAuthServiceProvider.php
+++ src/JeroenG/LaravelAuth/LaravelAuthServiceProvider.php
@@ -19,6 +19,17 @@ class LaravelAuthServiceProvider extends ServiceProvider {
 	public function boot()
 	{
 		$this->package('jeroen-g/laravel-auth');
+
+		// Extending the default Auth of Laravel.
+		\Auth::extend('eloquent', function()
+		{
+			// Retrieving the user model.
+			$model = $this->app['config']['auth.model'];
+			// Creating an Eloquent provider for the Guard class, which requires the app's hashing class and model from above.
+        	$provider = new Providers\EloquentUserProvider($this->app['hash'], $model);
+        	// This creates the new, extended Guard class, really the core of this package. It requires the app's session storage class.
+			return new Guard($provider, \App::make('session.store'));
+		});
 	}
 
 	/**
@@ -28,7 +39,12 @@ class LaravelAuthServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		//
+		// When using 'UserRepository', Laravel automatically uses the EloquentUserRepository
+		$this->app->bind('Repositories\UserRepository', 'JeroenG\LaravelAuth\Repositories\EloquentUserRepository');
+		// The same for Roles
+		$this->app->bind('Repositories\RoleRepository', 'JeroenG\LaravelAuth\Repositories\EloquentRoleRepository');
+		// And for Permissions
+		$this->app->bind('Repositories\PermissionRepository', 'JeroenG\LaravelAuth\Repositories\EloquentPermissionRepository');
 	}
 
 	/**
