140c90877f2f89070a9c0be1b97c98be80d29b0c
JeroenG <jeroengjeroeng@gmail.com>
2014-03-04 16:48:06 +0100

    Updated: added email column to users table.

--- src/migrations/auth_0_0_1_create_users_table.php
+++ src/migrations/auth_0_0_1_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('username')->unique();
+			$table->string('email')->unique();
 			$table->text('password');
 			$table->timestamps();
 			$table->softDeletes();
