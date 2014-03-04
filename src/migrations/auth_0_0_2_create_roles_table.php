72d3c15333edd6f32c3e1be89d1ca5eef29a7e44
JeroenG <jeroengjeroeng@gmail.com>
2014-03-04 16:50:19 +0100

    Updated: Added level column to roles table

--- src/migrations/auth_0_0_2_create_roles_table.php
+++ src/migrations/auth_0_0_2_create_roles_table.php
@@ -17,6 +17,7 @@ class CreateRolesTable extends Migration {
 			$table->increments('id');
 			$table->string('name')->unique();
 			$table->string('description')->nullable();
+			$table->smallInteger('level')->default(0);
 			$table->timestamps();
 			$table->softDeletes();
 		});
